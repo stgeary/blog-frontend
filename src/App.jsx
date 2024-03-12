ece0ad6c062fd4154142a647949984fbebf488f5
stgeary <scottgeary@Scotts-MacBook-Pro.local>
2024-03-12 14:11:05 -0500

    Added functions for all content pieces

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,80 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import reactLogo from './assets/react.svg';
+import './App.css';
+
+function Header() {
+  return (
+    <header>
+      <a href="#">Home</a> | <a href="#posts-index">All posts</a> | <a href="#posts-new">New post</a>
+    </header>
+  );
+}
+
+function PostsNew() {
+  return (
+    <div id="posts-new">
+      <h1>New post</h1>
+      <form>
+        <div>
+          Post text: <input type="text" />
+        </div>
+        <div> 
+          Author: <input type="text" />
+        </div>
+        <button type="submit">Be judged</button>
+      </form>
+    </div>
+  );
+}
+
+function PostsIndex(props) {
+  console.log("The props are", props);
+  return (
+    <div id="posts-index">
+      <h1>All posts</h1>
+      {/* <p>The name is {props.name}</p> */}
+      <div className="posts">
+        <h2>Scott</h2>
+        <p>I GOT SOME GUTTERS IN HERE WOOOHOOOOOO</p>
+      </div>
+      <div className="posts">
+        <h2>Scott</h2>
+        <p>I need gutters NOWWWWWWW</p>
+      </div>
+    </div>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>
+      <p>Copyright ∞, try suing me I dare you</p>
+    </footer>
+  );
+}
+
+function Content() {
+  let name = "Test";
+  return (
+    <>
+      <PostsNew />
+      <PostsIndex name={name} />
+    </>
+  );
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-      <header>
-
-        <a href="#">Home</a> | <a href="#posts-index">All posts</a> | <a href="#posts-new">New post</a>
-
-      </header>
-
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-
+        <Header />
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-
-      <div id="posts-new">
-        <h1>New post</h1>
-        <form>
-          <div>
-            Post text: <input type= "text" />
-          </div>
-          <div> 
-            Author: <input type="text" />
-          </div>
-          <button type="Submit">Be judged</button>
-        </form>
-      </div>
-
-      <div id="posts-index">
-        <h1>All posts</h1>
-      </div>
-
-      <h1>Scott requires gutters, NOWWWW</h1>
-
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 2 *1.72)}>
-          count is {count}
-        </button>
-
-      </div>
-      <footer>
-        <p>Copyright ∞, try suing me I dare you</p>
-      </footer>
+      <Content />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
