c8bcb7308f835a672016ec9540f81d0a407e75ca
stgeary <scottgeary@Scotts-MacBook-Pro.local>
2024-03-13 14:01:30 -0500

    Added colored bg for cards

--- src/content.jsx
+++ src/content.jsx
@@ -8,7 +8,7 @@ export function Content() {
         id: 1,
         title: "Scott",
         body: "Gutters created woooohoooo",
-        image_url: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages.ctfassets.net%2Fsfnkq8lmu5d7%2F5O3T6I1KjJ2IcLSRxhHJVp%2Facbe8ef2858eb0c98de93b061a531f52%2F2023-08-01_World_dog_surfing_championship_-_hero_.jpg&tbnid=DcnI-EsmF3iYbM&vet=12ahUKEwiWqqqx4PGEAxWKyckDHasrD_4QMygUegUIARCfAQ..i&imgrefurl=https%3A%2F%2Fwww.thewildest.com%2Fdog-lifestyle%2Fworld-dog-surfing-championships&docid=nEHbk8acaCHobM&w=1000&h=750&q=dog%20surfing&client=safari&ved=2ahUKEwiWqqqx4PGEAxWKyckDHasrD_4QMygUegUIARCfAQ.png",
+        image_url: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages.ctfassets.net%2Fsfnkq8lmu5d7%2F5O3T6I1KjJ2IcLSRxhHJVp%2Facbe8ef2858eb0c98de93b061a531f52%2F2023-08-01_World_dog_surfing_championship_-_hero_.jpg&tbnid=DcnI-EsmF3iYbM&vet=12ahUKEwiWqqqx4PGEAxWKyckDHasrD_4QMygUegUIARCfAQ..i&imgrefurl=https%3A%2F%2Fwww.thewildest.com%2Fdog-lifestyle%2Fworld-dog-surfing-championships&docid=nEHbk8acaCHobM&w=1000&h=750&q=dog%20surfing&client=safari&ved=2ahUKEwiWqqqx4PGEAxWKyckDHasrD_4QMygUegUIARCfAQ",
       },
       {
         id: 2,
