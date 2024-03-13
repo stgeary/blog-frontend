8a53a547f08266e00a888221f125319df7001bca
stgeary <scottgeary@Scotts-MacBook-Pro.local>
2024-03-13 15:48:22 -0500

    Posts now display on the front end

--- src/content.jsx
+++ src/content.jsx
@@ -1,19 +1,45 @@
 import {PostsNew} from "./postsnew"
 import {PostsIndex} from "./postsindex"
 import axios from "axios";
+import { useState } from "react";
 
 export function Content() {
     // let name = "Scott"
-    let posts = [];
+    
+    // let posts = [];
+
+    // let posts = [
+    //     {
+    //       id: 1,
+    //       title: "___",
+    //       body: "___",
+    //       image: "___",
+    //     },
+    //     {
+    //       id: 2,
+    //       title: "___",
+    //       body: "___",
+    //       image: "___",
+    //     },
+    //     {
+    //       id: 3,
+    //       title: "___",
+    //       body: "___",
+    //       image: "___",
+    //     },
+    //   ];
+
+    
+    const [posts, setPosts] = useState([]);
 
     const handleIndexPosts = () => {
         axios.get("http://localhost:3000/posts.json").then((response) => {
-            posts = response.data;
+            setPosts(response.data);
             console.log(posts);
         });
     };
     return (
-      <>
+        <>
         <PostsNew />
         <button onClick = {handleIndexPosts}>Load Posts</button>
         <PostsIndex posts={posts}/>
