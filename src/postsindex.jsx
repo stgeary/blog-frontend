1e45db1879da39e0d2aea84965ef07782f6206e3
stgeary <scottgeary@Scotts-MacBook-Pro.local>
2024-03-13 12:52:27 -0500

    Cards added successfully

--- src/postsindex.jsx
+++ src/postsindex.jsx
@@ -4,24 +4,18 @@ export function PostsIndex(props) {
     console.log(props);
   
     return (
-      <div>
-        {props.posts.map(post => (
-          <div key={post.id}>
-            <h2>{post.title}</h2>
-            <p>{post.body}</p>
-            <img src={post.image} alt={post.title} />
-          </div>
-        ))}
-            <div className="cards">
-        {props.posts.map((post) => (
-          <div key={post.id} className="posts card">
-            <h2>{post.title}</h2>
-            <img src={post.image_url} alt="" />
-            <p>Author:{post.Author} </p>
-            <button>More info</button>
-          </div>
-        ))}
-      </div>
+        <div id="posts-index">
+        <h2>The name is {props.name}</h2>
+        <h1>All posts</h1>
+        <div className="cards">
+          {props.posts.map((post) => (
+            <div key={post.id} className="posts card">
+              <h2>{post.title}</h2>
+              <p>{post.body}</p>
+              <img src={post.image_url} alt="" />
+            </div>
+          ))}
+        </div>
       </div>
     );
   }
